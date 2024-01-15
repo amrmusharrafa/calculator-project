7fe46e0947c2cd8aaa2e4f71d138f2a0bdc3049c
Amr Musharrafa <76596608+amrmusharrafa@users.noreply.github.com>
2024-01-15 20:07:40 +0200

    Update script.js
    handling error
    handling decimal point
    adding enter key event

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const operandButtons = document.querySelectorAll('button[type="operand"]');
 //grab operator buttons
 const operatorButtons = document.querySelectorAll('button[type="operatorbutton"]');
 
+
 //grab clear, allclear and equals buttons
  const clear = document.getElementById('clearbutton');
  const equals = document.getElementById('equalbutton');
@@ -34,10 +35,14 @@ const operatorButtons = document.querySelectorAll('button[type="operatorbutton"]
   operandButtons.forEach((number) => {
    number.addEventListener('click', (e) => {
 
+    // Check if decimal point is already present
+    if (e.target.value === '.' && userInput.includes('.')) {
+      return;}
     //if something exist on display delete it
     if (display_value.textContent && display_value.textContent === "0.") {
         display_value.textContent = display_value.textContent.slice(0, -2);
       }
+
        number = e.target.value;
        userInput.push(number);
        console.log(userInput);
@@ -77,20 +82,34 @@ allclear.addEventListener('click', () => {
 
 //handle equals button click
 equals.addEventListener('click', () => {
-  // check division by zero is invalid  
-   
-  display_value.textContent = eval(userInput.join(''));
-  userInput = [];
-  console.log(userInput);
+  try {
+    // Use the eval() function for simplicity (since it's a calculator app)
+    const result = eval(userInput.join(''));
+
+    // Check for valid result
+    if (!isNaN(result) && isFinite(result)) {
+        display_value.textContent = result;
+    } else {
+        display_value.textContent = 'Error';
+    }
+} catch (error) {
+    display_value.textContent = 'Error';
+}
+
+userInput = [];
 });
 
+document.addEventListener('keydown', (event) => {
+  let key = event.key;
+  const validKeys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '+', '-', '*', '/', 'Enter'];
 
+  if (key === 'Enter') {
+    event.preventDefault();
+    
+    equals.click();
+  }
 
-
-
-
-
-
-
-
-
+  if (validKeys.includes(key)) {
+    document.querySelector(`button[value="${key}"]`).click();
+  }
+});
